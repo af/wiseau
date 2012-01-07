2cde5dbfa7c6fb5127c73656b0393c91fe4657f2
Aaron Franks <aaron.franks@gmail.com>
2012-01-07 10:58:10 -0800

    Add tests for loading config files.

--- test/config.js
+++ test/config.js
@@ -3,13 +3,15 @@ var path = require('path');
 var assert = require('chai').assert;
 var BuildConfig = require('../lib/config').BuildConfig;
 
-var fixturesDir = path.resolve(__dirname, 'fixtures');
+var test_paths = {
+    fixtureDir: path.resolve(__dirname, 'fixtures'),
+    loadTest:   path.resolve(__dirname, 'fixtures', 'load_test.json'),
+    createTest: path.resolve(__dirname, 'fixtures', 'create_test.json')
+}
 
 describe('BuildConfig', function() {
-    var test_path = path.resolve(fixturesDir, 'testconfig.json');
-
-    it('creates a new config file with create()', function() {
-        var bc = BuildConfig.create(test_path);
+    it('creates a new config file', function() {
+        var bc = BuildConfig.create(test_paths.createTest);
         assert.ok(bc);
 
         // Make sure the created config file contains basic attributes:
@@ -21,10 +23,25 @@ describe('BuildConfig', function() {
         assert.length(Object.keys(bc.targets), 1);
     });
 
+    it('loads existing config files', function() {
+        var bc = BuildConfig.load(test_paths.loadTest);
+        assert.ok(bc);
+        assert.ok(bc.data);
+        assert.equal(bc.data.sourceDir, 'js/src');
+        assert.length(Object.keys(bc.targets), 1);
+    });
+
+    it('fails correctly when a specified config file does not exist', function() {
+        var fn = function() {
+            var bc = BuildConfig.load('invalid_name');
+        };
+        assert.throws(fn, Error);
+    });
+
     afterEach(function() {
         // Remove our test config file, if it was created:
         try {
-            fs.unlinkSync(test_path);
+            fs.unlinkSync(test_paths.createTest);
         }
         catch (error) {}
     });
